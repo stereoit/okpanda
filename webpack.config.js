016604a38a3460cfb8887fae6251aa13332d068a
Robert Smol <rsmol@localhost.localdomain>
2016-09-24 10:37:13 +0200

    Added HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,25 @@
-const {
-    resolve
-} = require('path')
+const {resolve} = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = env => {
-    return {
-        entry: './main.js',
-        output: {
-            path: resolve(__dirname, 'dist'),
-            filename: 'app.bundle.js',
-            pathinfo: !env.prod,
-        },
-        context: resolve(__dirname, 'src'),
-        module: {
-            loaders: [{
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel'
-            }, ]
-        }
-    }
+  return {
+    entry: './main.js',
+    output: {
+        path: resolve(__dirname, 'dist'),
+        filename: 'app.bundle.[chunkhash].js',
+        pathinfo: !env.prod,
+    },
+    context: resolve(__dirname, 'src'),
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel'
+        }, ]
+    },
+    plugins:[
+      new HtmlWebpackPlugin({
+        template: './index.html'
+      })
+    ]
+  }
 }
